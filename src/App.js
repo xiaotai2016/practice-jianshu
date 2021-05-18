d30096460781fbad62c82846f1638978ba14ce9c
xiaotai2016 <bowens@adbsystems.com.au>
2021-05-18 23:32:49 +1000

    add style-components

--- src/App.js
+++ src/App.js
@@ -1,43 +1,21 @@
 
+import React from 'react';
 import { connect } from 'react-redux';
 import { getInputValueAction, addListAction, deleteItemAction } from './store/createAction'
+import { Button } from "./style"
 
-function App({inputValue,setInputValue,list,setList,deleteItem}) {
+function App() {
 
   return (
-    <div>
-      <div>
-        <input value={inputValue} onChange ={setInputValue}/>
-        <button onClick ={setList} >submit</button>
-      </div>
-      <div>
-        <ul>
-          {
-            list.map((item,index)=>{
-              return <li onClick = {()=>{deleteItem(index)}}>{item}</li>
-              })
-                }
-                </ul>
-                </div>
-                </div>
-                );
-              }
+    <React.Fragment>
 
-              const mapStateToProps = (state) =>{
-                console.log(1,state);
-                return{
-                  inputValue:state.inputValue,
-                  list:state.list
-                }
-              }
-
-              const mapDispatchToProps = (dispatch) =>{
-                return{
-                  setInputValue: (e)=> dispatch(getInputValueAction(e.target.value)),
-                  setList:()=> dispatch(addListAction()),
-                  deleteItem: (index)=>dispatch(deleteItemAction(index))
+      hello world
+      <Button>I'm purple.</Button>
+    </React.Fragment>
+  )
   }
-}
 
 
-export default connect(mapStateToProps, mapDispatchToProps) (App);
+
+
+export default App;
---
+++ src/style.js
@@ -0,0 +1,11 @@
+//import { createGlobalStyle, styled } from 'styled-components'
+
+import styled from 'styled-components'
+
+
+export const Button = styled.div`
+  background: palevioletred;
+  border-radius: 3px;
+  border: none;
+  color: white;
+`
